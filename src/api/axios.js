122a42b1098f280d0b47a9467fe9102ae4eeac85
Avinash Mudunuri <avinashmudunuri@gmail.com>
2024-08-21 12:32:38 +0000

    updating baseurl path as per netlify

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export default axiosInstance;

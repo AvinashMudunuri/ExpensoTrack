473af22cf52ccce44ce49e65a2ac1cf1bfd9a838
Avinash Mudunuri <avinashmudunuri@gmail.com>
2024-08-21 12:41:41 +0000

    updated

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
 
-const Home = () => <Header title="Dashboard" />;
+const Home = () => <Header title="Dashboard here" />;
 
 export default Home;

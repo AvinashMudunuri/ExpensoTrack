2b415de57e3d9426c8187307d114a858ec9624c2
Avinash Mudunuri <avinashmudunuri@gmail.com>
2024-08-21 12:51:43 +0000

    cleanup and logger

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
+console.log('BASE URL:', process.env.REACT_APP_BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
